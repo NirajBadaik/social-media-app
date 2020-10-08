8294a59989a7e2e5b8e8191a78fb81c3af22da69
Niraj <nirajbadaik@gmail.com>
2020-10-08 19:53:53 +0530

    useReducer cange to useImmerReducer

--- app/main.js
+++ app/main.js
@@ -23,20 +23,20 @@ const Main = () => {
     flashMessages: [],
   };
 
-  function ourReducer(state, action) {
+  function ourReducer(draft, action) {
     switch (action.type) {
       case "login":
-        return { loggedIn: true, flashMessages: state.flashMessages };
+        draft.loggedIn = true;
+        return;
       case "logout":
-        return { loggedIn: false, flashMessages: state.flashMessages };
+        draft.loggedIn = false;
+        return;
       case "flashMessages":
-        return {
-          loggedIn: state.loggedIn,
-          flashMessages: state.flashMessages.concat(action.value),
-        };
+        draft.flashMessages.push(action.value);
+        return;
     }
   }
-  const [state, dispatch] = useReducer(ourReducer, initialState);
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
 
   // const [isLoggedin, setIsLoggedIn] = useState(
   //   Boolean(localStorage.getItem("socialFinderToken"))
